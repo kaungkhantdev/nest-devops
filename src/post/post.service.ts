5fa5894fb14ea2c259e931464d85d4a4ddaa66d2
kaungkhantcodigo <kaungkhantzaw@codigo.co>
2025-09-16 13:19:49 +0700

    add job lint

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,7 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { CreatePostDto } from './dto/create-post.dto';
